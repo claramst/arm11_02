0554b8c818a5d9d664dcedf6e30987a8109857cd
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2020-05-28 18:21:32 +0100

    Interface for bitmanipulation.c and introduces typedef alias for representing a byte.

---
+++ src/binary_utils/bitmanipulation.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+#ifndef BITMANIPULATION_H
+#define BITMANIPULATION_H
+
+typedef uint8_t BYTE;
+
+int getBits(int bin, int noOfBits, int n);
+int clearBits(int bin, int noOfBits, int n);
+int setBits(int bin, int noOfBits, int n);
+int getBit(int bin, int n);
+int getNibble(int bin, int n);
+int getByte(int bin, int n);
+int arithmeticRight(int bin, int n);
+int rotateRight(int bin, int n);
+int clearBits(int bin, int noOfBits, int n);
+int signExtend(int bin, int n);
+
+#endif
+
+
