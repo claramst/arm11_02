9c0ac7f9adb5a701a6c7c967dc978d1d7b635103
Stoddart, Clara <clara.stoddart19@imperial.ac.uk>
2020-05-29 15:03:28 +0100

    Execute file containing execute single data transfer function

---
+++ src/emulator_utils/execute.c
@@ -0,0 +1,51 @@
+#include "../../binary_utils/bitmanipulation.h"
+#include "../state.h"
+#include "instruction.h"
+
+
+
+void executeSingleTransfer(REGISTER* baseReg, REGISTER* targetReg, REGISTER* rmReg, int preBit, int upBit, int offset, MACHINE_STATE state) {
+  ADDRESS address = NULL;
+  if (decoded.P) {
+    // Pre-indexing: the offset is added/subtracted to the
+    // base register before transferring the data. Does not
+    // change base register value
+    if (upBit) {
+      // Offset added
+      address = *baseReg + offset;
+    } else {
+      // Offset subtracted
+      address = *baseReg - offset; 
+    }
+  } else {
+    // Post-indexing: the offset is added/subtracting after
+    // transferring the data. Changes base register value.
+    // A post-indexing ldr or str in which rm is the same 
+    // register as Rn, is not allowed.
+    assert(rmReg != baseReg);
+    address = *baseReg;
+    if (upBit) {
+      *baseReg += offset;
+    } else {
+      *baseReg -= offset;
+    }
+  } 
+  if (address > MAX_ADDRESSES) {
+    // Handles case where address is bigger than 16 bits
+    fprintf(stderr, "Address is too large");
+    exit();
+  }
+  address32bit = (ADDRESS) address;
+  if (decoded.L) {
+    // Load instruction
+    if (baseReg == pc) {
+      // The spec says we need to ensure in this case
+      // that pc holds the address of the current instruction
+      // plus 8. 
+    }
+    *targetReg = getWord(address32bit, state);
+  } else {
+    // Store instruction
+    storeWord(*targetReg, address32bit, state);
+  }
+}
