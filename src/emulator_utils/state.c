fef1e304c3def39cc4993f58732204d524812165
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2020-05-29 10:38:48 +0100

    Contains functions to load or store words in memory based on the given address and machine state.

---
+++ src/emulator_utils/state.c
@@ -0,0 +1,20 @@
+#include "instruction.h"
+#include "bitmanipulation.h"
+#include "state.h"
+
+// This function returns the word from memory 
+// at the given address (in big endian)
+WORD getWord(ADDRESS address, MACHINE_STATE state) {
+  int32_t sum = 0;
+  for (int i = 0; i < 4; i++) {
+    sum += state.memory[address + i] << 8 * i;
+  }
+  return sum;
+}
+
+// Stores the given word at the address given in big endian
+void storeWord(WORD word, ADDRESS address, MACHINE_STATE state) {
+  for (int i = 0; i < 4; i++) {
+    state.memory[address + i] = getByte(word, i);
+  }
+}
