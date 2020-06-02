79363064744b60091960df93d4409263914804be
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2020-06-02 15:44:06 +0100

    [rp619] Modified decodeInstr structure to use a union for the different data required by different instructions to save memory.

--- src/emulator_utils/decode.h
+++ src/emulator_utils/decode.h
@@ -18,22 +18,35 @@ typedef enum opcode {
 } OPCODE;
 
 typedef struct decodedInstr {
-    int condition;
-    enum instrType type;
-    int I;
-    int A;
-    int S;
-    int P;
-    int U;
-    int L;
-    unsigned int rn;
-    unsigned int rd;
-    unsigned int rs;
-    unsigned int rm;
-    int offset;
-    OPCODE opcode;
-    unsigned int op2;
-    unsigned int shiftCarryOut;
+  enum instrType type;
+  int condition;
+  unsigned int rn;
+  unsigned int rd;
+  unsigned int shiftCarryOut;
+  union {
+    struct {
+      int I;
+      int S;
+      OPCODE opcode;
+      unsigned int op2;
+    };
+    struct {
+      int A; 
+      int S;
+      int rs;
+      int rm;
+    };
+    struct {
+      int I;
+      int P;
+      int U;
+      int L;
+      int offset;
+    };
+    struct {
+      int offset;
+    };
+  };
 } DECODED_INSTR;
 
 DECODED_INSTR decode(INSTRUCTION instr, MACHINE_STATE state);
